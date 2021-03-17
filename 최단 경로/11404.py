ae1b7da4a0d6d779f8a61035911031f579e6d7f6
lee-moo <dlandif22@gmail.com>
2021-03-17 23:40:08 +0900

    최단 경로

--- 최단 경로/11404.py
+++ 최단 경로/11404.py
@@ -5,5 +5,29 @@ import sys
 
 n = int(input())
 m = int(input())
+INF = int(1e9)
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
 
-graph = [[] for _ in range(n+1)]
+
+
+for i in range(n+1) :
+    graph[i][i] = 0
+
+for _ in range(m) :
+    a, b, c = map(int,input().split())
+    if graph[a][b] > c : 
+        graph[a][b] = c
+
+
+for k in range(1, n+1) :
+    for i in range(1,n+1) :
+        for j in range(1, n+1) :
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])              
+
+for i in range(1,n+1) :
+    for j in range(1,n+1) :
+        if graph[i][j] == INF :
+            print(0, end = ' ')
+        else :
+            print(graph[i][j], end =' ')
+    print();
