e6a011993448c46eb141368112a2c21df9f3a9c3
dlandif22 <dlandif22@gmail.com>
2021-06-28 13:00:55 +0900

    binary sort

--- sort/binary search/가사검색.py
+++ sort/binary search/가사검색.py
@@ -4,22 +4,43 @@
 # 가사에 사용된 모든 단어들이 담긴 배열 words와 찾고자 하는 키워드가 담긴 배열 queries
 
 # 고려 사항 : 문자 길이, 와일드 카드의 위치, 문자 검색, 이진 탐색
+
+from bisect import bisect_left, bisect_right
+
+
+def count_by_range(str, left_value, right_value):
+    left = bisect_left(str, left_value)
+    right = bisect_right(str, right_value)
+    return right - left
+
+
+array = [[] for _ in range(10001)]
+reversed = [[] for _ in range(10001)]
+
+
 def solution(words, queries):
     answer = []
-    words.sort()
-    words_length = []
-    for i in range(words):
-        words_length.append(len(words[i]))
 
-    # 검색의 기준이 와일드 카드와 문자가 골고루 섞인 유형과
-    # 모든 문자가 와일드 카드로만 구성된 것도 생각해서 구현해야 한다.
-    for i in range(len(queries)):
-        query = queries[i]
-        query_length = len(query)
+    for word in words:
+        array[len(word)].append(word)
+        reversed[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed[i].sort()
+
+    for query in queries:
+        if query[0] == '?':
+            result = count_by_range(reversed[len(
+                query)], query[::-1].replace('?', 'a'), query[::-1].replace('?', 'z'))
+        else:
+            result = count_by_range(array[len(query)], query.replace(
+                '?', 'a'), query.replace('?', 'z'))
+
+        answer.append(result)
+
     return answer
 
 
-words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
-queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
-
-solution(words, queries)
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], [
+      "fro??", "????o", "fr???", "fro???", "pro?"]))
