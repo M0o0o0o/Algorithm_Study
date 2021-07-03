0e806271d92284e3cacee113e2b1db2945ea4c02
dlandif22 <dlandif22@gmail.com>
2021-07-03 20:40:28 +0900

    graph

--- 최단 경로/정확한순위.py
+++ 최단 경로/정확한순위.py
@@ -1,23 +1,29 @@
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 
 INF = int(1e9)
-graph = [[0]* (n+1) for _ in range (n+1)]
+graph = [[INF]*(n+1) for _ in range(n+1)]
 
-rank = [[0]*2 for _ in range(n+1)]
+for i in range(1, n+1):
+    graph[i][i] = 0
 
-for _ in range(m) :
+for _ in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
 
-for i in range(1, n+1) :
-    for j in range(1, n+1) :
-        if graph[i][j] == 1 :
-            rank[i][1] += (1 + rank[j][1])
-            rank[j][0] += (1 + rank[i][0])
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
 
 result = 0
-for i in range(1, n+1) :
-    if sum(rank[i]) >= (n-1) :
+for i in range(1, n+1):
+    total = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            total += 1
+
+    if total == n:
         result += 1
 
 print(result)
