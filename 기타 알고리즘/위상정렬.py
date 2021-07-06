38f4027d29092e31a7bb6cfa0413ef33e0de0b66
dlandif22 <dlandif22@gmail.com>
2021-07-06 20:47:20 +0900

    [ADD] etc

--- 기타 알고리즘/위상정렬.py
+++ 기타 알고리즘/위상정렬.py
@@ -6,28 +6,30 @@ indgree = [0] * (v + 1)
 
 graph = [[] for i in range(v + 1)]
 
-for _ in range(e) :
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  indgree[b] += 1
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indgree[b] += 1
 
-def topology_sort() :
-  result = []
-  q = deque()
 
-  for i in range(1, v + 1) :
-    if indgree[i] == 0 :
-      q.append(i)
+def topology_sort():
+    result = []
+    q = deque()
 
-  while q:
-    now = q.popleft()
-    result.append(now)
-    for i in graph[now] :
-      indgree[i] -= 1
-      if indgree[i] == 0 :
-        q.append(i)
+    for i in range(1, v + 1):
+        if indgree[i] == 0:
+            q.append(i)
 
-  for i in result : 
-    print(i, end = ' ')
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for i in graph[now]:
+            indgree[i] -= 1
+            if indgree[i] == 0:
+                q.append(i)
+
+    for i in result:
+        print(i, end=' ')
+
 
 topology_sort()
