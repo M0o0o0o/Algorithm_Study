0e3fc579b7715f9c276f9c38a50487dc6a04ecdc
Lee moo yeol <dlandif22@gmail.com>
2021-07-31 15:15:19 +0900

    [ADD] acmicpc 5086

---
+++ etc/acmicpc2164.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+n = int(input())
+lst = deque([i for i in range(1, n+1)])
+
+while len(lst) > 1 :
+    lst.popleft()
+    lst.append(lst.popleft())
+
+print(lst[0])
