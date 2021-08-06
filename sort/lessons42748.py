e772d83935f7710d71ebe3fdd9e959042f330ff8
Lee moo yeol <dlandif22@gmail.com>
2021-08-06 20:19:19 +0900

    [ADD] lessons42748.

---
+++ sort/lessons42748.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for i,j,k in commands  :
+        buf = array[i-1: j]
+        buf.sort()
+        answer.append(buf[k-1])
+    return answer
+
+print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
