eb18731258abf2683a840513abfe2fbbb0ffe1c9
Lee moo yeol <dlandif22@gmail.com>
2021-08-24 10:11:11 +0900

    [ADD] problem5585

---
+++ Greedy/problem5585.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/5585
+
+coin = 1000 - int(input())
+answer = 0
+money = [500,100,50,10,5,1]
+
+while coin > 0 :
+    for m in money :
+        if coin // m >= 1 :
+            answer += coin // m 
+            coin %= m
+            
+print(answer) 
+    
