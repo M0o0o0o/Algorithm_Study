5dafce096d6b176651b1eae84cef8f6662edbe54
Lee moo yeol <dlandif22@gmail.com>
2021-08-28 10:11:11 +0900

    [ADD] problem5585

--- Greedy/problem5585.py
+++ Greedy/problem5585.py
@@ -4,11 +4,11 @@ coin = 1000 - int(input())
 answer = 0
 money = [500,100,50,10,5,1]
 
-while coin > 0 :
-    for m in money :
-        if coin // m >= 1 :
-            answer += coin // m 
-            coin %= m
-            
+
+for m in money :
+    if coin // m >= 1 :
+        answer += coin // m 
+        coin %= m
+
 print(answer) 
     
