dc70e317271d937bca7b64d58345a5c01c38d6eb
Lee moo yeol <dlandif22@gmail.com>
2021-08-29 10:11:11 +0900

    [ADD] problem16953

---
+++ Greedy/problem16953.py
@@ -0,0 +1,19 @@
+a, b = map(int,input().split())
+answer = int(1e9) 
+
+def solution(a, b, count) :
+    global answer
+    if a > b :
+        return
+    if a == b :
+        answer = min(answer, count)
+    solution(a * 2, b, count + 1) 
+    solution(int(str(a) + "1"), b, count + 1) 
+
+    return 
+
+solution(a,b,0)
+if answer == int(1e9) :
+    print(-1) 
+else : 
+    print(answer + 1 )
