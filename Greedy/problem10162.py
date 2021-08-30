e5b5ea685ad8b1844f8e8d0d9b6175a0dfef29db
Lee moo yeol <dlandif22@gmail.com>
2021-08-30 10:11:11 +0900

    [ADD] problem10162

--- Greedy/problem10162.py
+++ Greedy/problem10162.py
@@ -1,19 +1,14 @@
 # https://www.acmicpc.net/problem/10162
 
 time = int(input())
-btn = [[300, 0], [60, 0], [10,0]]
-possible = True
+btn = [300,60,10]
 
 if time % 10 != 0 :
-    possible = False
-
-while possible and time > 0 :
-    for i in range(len(btn)) :
-        if time // btn[i][0] >= 1 :
-            btn[i][1] += time // btn[i][0]
-            time %= btn[i][0]
-
-if possible :
-    print(btn[0][1], btn[1][1], btn[2][1])
-else :
     print(-1)
+else :
+    for i in range(len(btn)) :
+        if time // btn[i] >= 1 :
+            print(time // btn[i], end = ' ')
+            time %= btn[i]
+        else :
+            print(0, end = ' ')
