faf7f3843efed08441d65a8addb39dca68f42bda
Lee moo yeol <dlandif22@gmail.com>
2021-09-07 10:11:11 +0900

    [ADD] problem 2747

--- math/problem2747.py
+++ math/problem2747.py
@@ -1,9 +1,9 @@
 # https://www.acmicpc.net/problem/2747
 
 n = int(input())
-fibo = [0] * (n+1)
+fibo = [0] * (1500001)
 fibo[1] = 1 
-for i in range(2, n+1) :
-    fibo[i] = fibo[i-1] + fibo[i-2]
+for i in range(2, 1500001) :
+    fibo[i] = (fibo[i-1] + fibo[i-2]) % 1000000
+print(fibo[n % 1500000] )
 
-print(fibo[-1])
