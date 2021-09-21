6d868f8c95593467002db58037e49918b643bdd8
Lee moo yeol <dlandif22@gmail.com>
2021-09-21 15:32:45 +0900

    [ADD] problem10773

---
+++ 구현/problem10773.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/10773
+
+k = int(input())
+lst = [] 
+
+for _ in range(k) :
+    money = int(input())
+    if money == 0 :
+        lst.pop()
+        continue
+    lst.append(money)
+
+print(sum(lst))
