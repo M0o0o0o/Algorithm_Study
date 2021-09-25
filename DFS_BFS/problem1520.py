6714eab74727c90938152abcc2f01acc14fc313d
Lee moo yeol <dlandif22@gmail.com>
2021-09-25 18:10:04 +0900

    [ADD] pproblem1520

--- DFS_BFS/problem1520.py
+++ DFS_BFS/problem1520.py
@@ -1,28 +1,24 @@
 # https://www.acmicpc.net/problem/1520
 
-from collections import deque
 n, m = map(int,input().split())
 answer = 0
-queue = deque([])
 graph = []
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
-
+dp = [[-1] * m for _ in range(n)]
 for _ in range(n) :
     graph.append(list(map(int,input().split())))
 
-queue.append((0,0))
-
-while queue : 
-    x, y = queue.popleft()
+def dfs(x,y) :
+    if (x,y) == (n-1, m-1) :
+        return 1
+    if dp[x][y] != -1 :
+        return dp[x][y]
+    dp[x][y] = 0 
     for i in range(4) :
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] < graph[x][y] :
-            if nx == n-1 and ny == m-1 :
-                answer += 1
-                break
-            queue.append((nx,ny))
+        if 0 <= nx < n and 0 <= ny < m  and graph[x][y] > graph[nx][ny]:
+            dp[x][y] += dfs(nx,ny) 
+    return dp[x][y]
 
-print(answer)
-
-
+print(dfs(0,0))
