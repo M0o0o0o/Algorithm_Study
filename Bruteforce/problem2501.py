945bf51a27e691a359a078ed28ded97c15f4f4f8
Lee moo yeol <dlandif22@gmail.com>
2021-09-30 15:01:08 +0900

    [ADD] problem2501

---
+++ Bruteforce/problem2501.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/2501
+
+n, k = map(int,input().split())
+
+lst = []
+for i in range(1, n+1) :
+    if n % i == 0 :
+        lst.append(i)
+
+if len(lst) >= k :
+    print(lst[k-1])
+else :
+    print(0)
