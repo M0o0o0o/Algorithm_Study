33cb8e19af6230b79714d3e9c5448e087e8cc562
Lee moo yeol <dlandif22@gmail.com>
2021-10-05 16:57:11 +0900

    [re]

--- etc_Algorithm/위상정렬.py
+++ etc_Algorithm/위상정렬.py
@@ -1,21 +1,24 @@
 from collections import deque
 
+# 노드의 개수와 간선의 개수를 입력받기
 v, e = map(int, input().split())
-
+# 모든 노드에 대한 진입차수는 0으로 초기화
 indgree = [0] * (v + 1)
-
+# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
 graph = [[] for i in range(v + 1)]
 
+
+# 방향 그래프의 모든 간선 정보를 입력받기
 for _ in range(e):
     a, b = map(int, input().split())
     graph[a].append(b)
     indgree[b] += 1
 
-
+# 위상 정렬 함수 
 def topology_sort():
     result = []
     q = deque()
-
+# 처음 시작할 때는 진입차수가 0인 노드를 큐에 append
     for i in range(1, v + 1):
         if indgree[i] == 0:
             q.append(i)
