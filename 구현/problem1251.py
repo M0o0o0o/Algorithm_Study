6decd93cc9889fe53bd95f932e78d412986bd867
Lee moo yeol <dlandif22@gmail.com>
2021-10-12 21:55:40 +0900

    [ADD] problem 1251

---
+++ 구현/problem1251.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1251
+
+s = input()
+answer = []
+
+for i in range(1,len(s)-2) :
+    for j in range(i+1, len(s)) :
+        a,b,c = s[:i], s[i:j], s[j:]
+        answer.append(a[::-1] + b[::-1] + c[::-1])
+      
+
+
+answer.sort()
+print(answer[0])
+#  0 1 2 3 4 5 6
+#  m o b i t e l
