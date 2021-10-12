7344343421698f52b90dd9077ac9450945dba12c
Lee moo yeol <dlandif22@gmail.com>
2021-10-12 20:27:35 +0900

    [ADD] problem 1292

---
+++ 구현/problem1292.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/1292
+
+lst = []
+a, b = map(int,input().split())
+answer = 0
+count = 0
+for i in range(1, 1001) :
+    for j in range(i) :
+        count += 1
+        if a <= count <= b :
+            answer += i
+
+print(answer)
+        
