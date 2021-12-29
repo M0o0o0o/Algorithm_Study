1db48485d4b07e144521cc032f17407b64399357
Lee moo yeol <dlandif22@gmail.com>
2021-12-29 17:42:14 +0900

    [ADD] 구현/problem23811.py

---
+++ 구현/problem23811.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/23811
+
+n = int(input())
+[print('@@@@@' * n) for _ in range(n)]
+[print('@' * n) for _ in range(n)]
+[print('@@@@@' * n) for _ in range(n)]
+[print('@' * n) for _ in range(n)]
+[print('@@@@@' * n) for _ in range(n)]
