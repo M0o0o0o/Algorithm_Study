9459fe36cfadcf1fc89f1ccc86356c567e5be081
Lee moo yeol <dlandif22@gmail.com>
2022-01-04 16:30:40 +0900

    [ADD] String/problem11575.py

---
+++ String/problem11575.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/11575
+
+for _ in range(int(input())) : 
+    a, b = map(int, input().split())
+    string = list(input())
+    for i in range(len(string)) :
+        string[i] = chr((a * (ord(string[i]) - 65) + b) % 26 + 65)
+    print(''.join(string))
+    
