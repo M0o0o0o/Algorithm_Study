b60eb32fd0d64cab55f451cc1ab7f512654ade70
lmy <dlandif22@gmail.com>
2022-01-22 13:19:49 +0900

    [fix]

--- shortestPath/problem11781.py
+++ shortestPath/problem11781.py
@@ -1,51 +1,53 @@
 # https://www.acmicpc.net/problem/11781
 
-import sys 
-input = sys.stdin.readline
 import heapq
+import sys
+input = sys.stdin.readline
 
-n, m, s, e = map(int, input().split())  
+n, m, s, e = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 INF = float('inf')
 distance = [INF] * (n+1)
 
-for _ in range(m) :
+for _ in range(m):
     a, b, l, t1, t2 = map(int, input().split())
-    graph[a].append((b,l,t1))
-    graph[b].append((a,l,t2))
-    
+    graph[a].append((b, l, t1))
+    graph[b].append((a, l, t2))
+
 q = []
-heapq.heappush(q, (0,1))
-distance[1] = 0 
+heapq.heappush(q, (0, 1))
+distance[1] = 0
 
-while q : 
+while q:
     dist, now = heapq.heappop(q)
-    if distance[now] < dist  :
+    if distance[now] < dist:
         continue
-    for node, length, jam in graph[now] : 
-        # 퇴근 시간 여부의 따른 cost 계산
-        # 퇴근 시간이 아닐 때에는 거리 1 이동하는데 1분 
-        # 퇴근 시간일 경우에는 거리 1 이동하는데 2분 
-        cost = dist + length 
-        if jam and ((dist <= s <= cost) or (dist <= e <= cost)):
-            cost_ = 0 
-            if dist <= s <= cost : 
-                cost_ = cost - s 
-            else : 
-                cost_ = cost - e
-            #cost_ = 3 
-            length_ = cost_ / 2  
-            #length_ = 1.5
-            if length_ < length :  
-                cost_ += length - length_  # 3 += (8 - )
-            elif length_ > length: 
-                cost_ -= (length_ - length) / 2
-            cost = cost_ + dist
-            if node ==2 : 
-                print('test :', cost_, dist)                 
-        if cost < distance[node] : 
-            distance[node] = cost 
-            heapq.heappush(q,(cost, node))
-            
+    for node, length, jam in graph[now]:
+        cost = dist + length
+        if jam == 1 and ((dist <= s <= dist + length) or (dist <= e <= dist + length)):
+            if dist <= s <= cost:
+                cost = dist + (s - dist)
+                remain_d = length - s  # 3
+                remain_m = e-s  # 8
+                remain_m_d = remain_m / 2  # 4
+                if remain_d <= remain_m_d:
+                    cost += remain_d * 2
+                else:
+                    cost += remain_m + (remain_d - remain_m_d)
+            else:  # 시작부터 퇴근시간에 걸리는 경우
+                minute = e - dist  # 5분
+                minute_d = minute / 2
+                cost = dist + minute + (length - minute_d)
+        if cost < distance[node]:
+            distance[node] = cost
+            heapq.heappush(q, (cost, node))
 
-print(distance)
+ans = 0
+for d in distance:
+    if d == float('inf'):
+        continue
+    ans = max(ans, d)
+if int(ans) == ans:
+    print(int(ans))
+else:
+    print(ans)
