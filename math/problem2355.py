eeba5cffedcdcb79ffabf9afa8d3dcd8329b05c5
lmy <dlandif22@gmail.com>
2022-02-25 07:51:39 +0900

    add

--- math/problem2355.py
+++ math/problem2355.py
@@ -1,8 +1,18 @@
 # https://www.acmicpc.net/problem/2355
 
-def sum_(num):
-    return (num * (num + 1)) // 2
-
-
 a, b = map(int, input().split())
-print(sum_(b) - sum_(a) - a)
+a, b = min(a, b), max(a, b)
+
+
+def f(n):
+    return (n*(n+1))//2
+
+
+if a == b:
+    print(a)
+elif a <= 0 and b >= 0:
+    print(f(b) - f(-a))
+elif b <= 0:
+    print(-(f(-a) - f(-b-1)))
+else:
+    print(f(b) - f(a-1))
