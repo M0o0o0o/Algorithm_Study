fa1c7b2a633e3a2896dec28cb8f4226dd2603884
이무열 <imuyeol@imuyeol-ui-MacBookAir.local>
2022-03-15 15:06:10 +0900

    add problem 14914

---
+++ math/problem14914.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/14914 
+
+n, m = map(int, input().split())
+
+for i in range(1, min(n,m) + 1):
+    if n % i == 0 and m % i == 0 : 
+        print(i, n//i, m//i)
+
+
