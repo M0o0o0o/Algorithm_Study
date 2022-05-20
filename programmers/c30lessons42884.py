2b9020a67ebf9c149d8a6c93f62f62b0c91066e3
Lee-moo <dlandif22@gmail.com>
2022-05-20 11:56:28 +0900

    fix programmer 42884

--- programmers/c30lessons42884.py
+++ programmers/c30lessons42884.py
@@ -2,5 +2,29 @@
 
 
 def solution(routes):
-    answer = 0
-    return answer
+    ans = 0
+    routes.sort(key=lambda x: x[1])
+    ll = len(routes)
+    check = [0] * ll
+
+    for i in range(ll):
+        if check[i] == 0:
+            time = routes[i][1]
+            ans += 1
+        for j in range(i+1, ll):
+            if routes[j][0] <= time <= routes[j][1] and check[j] == 0:
+                check[j] = 1
+    return ans
+
+
+def solution2(routes):
+    ans = 0
+    routes.sort(key=lambda x: x[1])
+    time = -30001
+
+    for r in routes:
+        if time < r[0]:
+            ans += 1
+            time = r[1]
+
+    return ans
