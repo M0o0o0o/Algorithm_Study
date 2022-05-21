b34ceab53d062f344cadcc7ed162d4974dc72f67
Lee-moo <dlandif22@gmail.com>
2022-05-21 22:25:23 +0900

    [ADD] math/problem3034.py

---
+++ math/problem3034.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/3034
+n, w, h = map(int, input().split())
+for i in range(n):
+    b = int(input())
+    l = (w ** 2 + h ** 2) ** 0.5
+    if b <= l:
+        print("DA")
+    else:
+        print("NE")
