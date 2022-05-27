a7524a6894ae3b6db4371d43c3f85b49a528168c
Lee-moo <dlandif22@gmail.com>
2022-05-27 17:54:39 +0900

    rm problem 2229

--- dynamicProgramming/problem2229.py
+++
@@ -1,9 +0,0 @@
-# https://www.acmicpc.net/problem/2229
-
-n = int(input())
-lst = [0] + list(map(int, input().split()))
-dp = [0] * (n+1)
-for i in range(1, n+1):
-    dp[i] = max(dp[i-1], abs(lst[i] - lst[i-1]) + dp[i-2])
-
-print(max(dp))
